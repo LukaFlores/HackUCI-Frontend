502b00a2ef646b49e05d8744b3a8e9e5e7b465b5
Katelyn Wang <katelyn.j.wang@gmail.com>
2022-02-26 12:06:32 -0800

    updated profile card

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import profileImg from '../assets/images/testProfilePhoto.jpeg';
+import profileImg2 from '../assets/images/stockprofile.jpg';
+import redX from '../assets/images/redx.png';
+import greenCheck from '../assets/images/greencheckmark.png';
 
 const ProfileCard: React.FC<{}> = (props) => {
+  // const profileData = {
+  //   name: 'Mary',
+  //   age: '15',
+  //   img: profileImg,
+  //   description: 'Hello my name is mary, I love to workout',
+  //   gender: 'female'
+  // };
+
   const profileData = {
-    name: 'Katelyn',
-    age: '15',
-    img: profileImg,
-    description: 'Hello my name is mary, I love to workout',
-  };
+    name: 'Ben',
+    age: 25,
+    img: profileImg2,
+    description: 'I love running and swimming and would love to find someone to workout with',
+    gender: 'male',
+    interests: 'running, swimming'
+  }
+
+  function disliked() {}
+  function liked() {}
+
 
   return (
     <div className="justify-center flex items-center m-40">
-      <div className="w-100 p-6 bg-white rounded-xl">
-        <div className='text-center text-black text-2xl font-bold'>
-            {profileData.name}
+      <div className="w-100 p-6 bg-white rounded-xl drop-shadow-md">
+        <div className='text-center text-black text-2xl'>
+            <b>{profileData.name}</b>, <i>{profileData.age}</i>
         </div>
         <img src={profileData.img} className="rounded-xl"/>
+        <div className='text-center text-black'>
+          {profileData.description}
+        </div>
+
+        <div className='text-center text-black'>
+          <b>Interests:</b> {profileData.interests}
+        </div>
+
+        <div className="flex items-baseline justify-between">
+          <img className='h-20 hover:bg-red-400 rounded-full' onClick={() => disliked}  src={redX}/>
+          <img className='h-20 hover:bg-green-1 rounded-full' onClick={() => liked} src={greenCheck}/>
+        </div>
+
+
       </div>
     </div>
   );
